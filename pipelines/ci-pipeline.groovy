a9cebe30154d168afbabb4eeda23208e69f89d48
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-04-29 10:00:41 +0100

    readjust wait times for build pipelines

--- pipelines/ci-pipeline.groovy
+++ pipelines/ci-pipeline.groovy
@@ -21,7 +21,9 @@ node('maven') {
 	}
 
 	stage("build & deploy container") {
-		sh "oc start-build ${microservice} --follow --wait --env JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -n ${project}"
+		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
+		sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -n ${project}"
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
 		sleep time:120 
 	}
 
